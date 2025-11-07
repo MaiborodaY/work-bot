afc4ca5a579916dbd4fdff0a61a1bcbe78433360
Maiboroda_E <maiboroda.e@effie.ai>
2025-11-08 00:36:21 +0100

    spin 1k fix

--- src/GameConfig.js
+++ src/GameConfig.js
@@ -51,7 +51,7 @@ export const CONFIG = {
 
   // Казино
   CASINO: {
-    prices: [5, 10, 20, 50, 100, 200, 500, 100],
+    prices: [5, 10, 20, 50, 100, 200, 500, 1000],
     price_low:  5,
     price_high: 10,
     cooldown_ms: 4_000,
